bc5011323059029666b2c04bc672653d36b92ce8
swk <swk98@naver.com>
2023-05-19 11:49:48 +0900

    commit

---
+++ src/com/company/P1316.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.*;
+
+public class P1316 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int answer = 0;
+
+        sc.nextLine();
+
+        for(int i=0;i<n;i++){
+            answer += check(sc.nextLine());
+        }
+
+        System.out.println(answer);
+
+    }
+
+    public static int check(String word){
+        List<Character> list = new ArrayList<>();
+        Set<Character> set = new HashSet<>();
+
+        list.add(word.charAt(0));
+        for(int i=1;i<word.length();i++){
+            if(word.charAt(i)!=word.charAt(i-1)){
+                list.add(word.charAt(i));
+            }
+        }
+
+        for(int i=0;i<list.size();i++){
+            set.add(list.get(i));
+        }
+
+        if(list.size()==set.size()){
+            return 1;
+        }else{
+            return 0;
+        }
+
+    }
+
+}
+
+
