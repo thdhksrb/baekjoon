e5434883567d85fa708363342b9b478ac7e23eea
swk <swk98@naver.com>
2023-08-22 16:49:08 +0900

    퀴즈풀이

--- src/com/company/quiz/Problem71.java
+++ src/com/company/quiz/Problem71.java
@@ -5,32 +5,27 @@ import java.util.*;
 public class Problem71 {
 
     public static int GCD(int a, int b){
-        int r = b%a;
-        if(r!=0){
-            return GCD(r,a);
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
         return a;
     }
     public static void main(String[] args) {
-        Map<String,Double> map = new HashMap<>();
 
-        for(int i=1;i<=1000000;i++){
-            for(int j=1;j<i;j++){
-                if(GCD(j,i)==1){
-                    map.put(j + "/" + i,(double)j/i);
-                }
+        int up = 0;   //분자
+        int down = 1; //분모
+
+        for (int d = 2; d <= 1000000; d++) { //새로운 분모 순환
+            int n = (3 * d - 1) / 7; //가장 비슷한 분자
+            if (GCD(n, d) == 1 && n * down > up * d) {
+                up = n;   //분자
+                down = d; //분모
             }
         }
 
-        List<Map.Entry<String,Double>> entries = new ArrayList<>(map.entrySet());
-        entries.sort((o1,o2)->o1.getValue().compareTo(o2.getValue()));
-
-        for(int i=0;i<entries.size();i++){
-            if(entries.get(i).getKey().equals("3/7")){
-                System.out.println(entries.get(i-1).getKey());
-                break;
-            }
-        }
+        System.out.println(up);
 
 
     }
