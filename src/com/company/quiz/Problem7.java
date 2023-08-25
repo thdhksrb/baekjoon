625d3e0fedb7bc9eca5b914290edeff1658dbd20
swk <swk98@naver.com>
2023-08-25 14:41:43 +0900

    퀴즈풀이

--- src/com/company/quiz/Problem7.java
+++ src/com/company/quiz/Problem7.java
@@ -6,6 +6,24 @@ import java.util.*;
 public class Problem7 {
     public static void main(String[] args) {
 
+        int count = 1;
+        for(int i=3;;i+=2){
+            boolean flag = true;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                count++;
+            }
+            if(count==10001){
+                System.out.println(i);
+                return;
+            }
+        }
+
 
     }
 }
